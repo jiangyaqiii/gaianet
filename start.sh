753595734057e7c4e50acdd7a5a9d86a57062375
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-09 16:50:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ curl -sSfL 'https://raw.githubusercontent.com/GaiaNet-AI/gaianet-node/main/insta
 cd ~
 source .wasmedge/env
 #"/root/gaianet/bin:$PATH"
-#ln -s /root/gaianet/bin /usr/local/bin/gaianet
+#ln -s /root/gaianet/bin/gaianet /usr/local/bin/gaianet
 export PATH=/root/gaianet/bin:$PATH
 alias gaianet='/root/gaianet/bin/gaianet'
 #source ~/.bashrc
