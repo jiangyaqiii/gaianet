df0217c3720b8587e1659ce3a4bb1ed67b84e2ee
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-09 14:26:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ cd ~
 source .wasmedge/env
 source .bashrc
 handle_error() {
+    echo '环境异常，重新生效环境文件'
     local last_command=$1
     local error_code=$2
     echo "Error: Command '$last_command' failed with error code $error_code"
