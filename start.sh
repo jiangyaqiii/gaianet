27884263588585c3746041dd6a0ca6a766f7522a
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-09 15:36:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 curl -sSfL 'https://raw.githubusercontent.com/GaiaNet-AI/gaianet-node/main/install.sh' | bash
 cd ~
 source .wasmedge/env
+#"/root/gaianet/bin:$PATH"
+#ln -s /root/gaianet/bin /etc/bin/link
+
 source ~/.bashrc
 gaianet init
 gaianet start
