f59f8b7adf98749ef51517c80279844cd0734d1e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-08 10:16:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+echo "\$nrconf{kernelhints} = 0;" >> /etc/needrestart/needrestart.conf
+echo "\$nrconf{restart} = 'l';" >> /etc/needrestart/needrestart.conf
+source /root/.bashrc
+
 sudo apt-get update -y
 sudo apt-get install libgomp1 -y
 curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
@@ -9,4 +13,6 @@ source /root/.bashrc
 
 gaianet init
 gaianet start
+cd
+rm start.sh
 
