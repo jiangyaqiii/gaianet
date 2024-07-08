cc6352239e54680e51d3c03523d89037f46497ea
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-08 10:41:47 +0800

    Create low_start.sh

---
+++ low_start.sh
@@ -0,0 +1,17 @@
+echo "\$nrconf{kernelhints} = 0;" >> /etc/needrestart/needrestart.conf
+echo "\$nrconf{restart} = 'l';" >> /etc/needrestart/needrestart.conf
+source /root/.bashrc
+
+sudo apt-get update -y
+sudo apt-get install libgomp1 -y
+curl -sSfL 'https://github.com/GaiaNet-AI/gaianet-node/releases/latest/download/install.sh' | bash
+
+source /root/.bashrc
+#"/root/gaianet/bin:$PATH"
+#ln -s /root/gaianet/bin/gaianet /usr/local/bin/gaianet
+#source ~/.bashrc
+
+gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json
+gaianet start
+cd
+rm start.sh
